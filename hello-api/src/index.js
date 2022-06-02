e12404eaa5cd8a35f43aac53e79199599d0c57bf
dodisutarya <ikhwands@protonmail.ch>
2022-06-02 11:15:38 +0700

    add how to start db.json

--- hello-api/src/index.js
+++ hello-api/src/index.js
@@ -9,3 +9,10 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+// cara untuk menjalankan database server 
+/*
+1. Pastikan db.json yang didalamnya terdapat data-data sudah ada.
+2. Install json-server dengan menggunakan perintah `npm install -g json-server`
+3. Jalankan json-server dengan menggunakan perintah `json-server --watch db.json --port 3002`
+*/
